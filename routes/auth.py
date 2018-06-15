ed7789157e056c9ba61df250271fdd39954019ab
Hasan <hasan.jafri7@gmail.com>
2018-06-15 00:30:02 -0500

    clean up code

--- routes/auth.py
+++ routes/auth.py
@@ -13,23 +13,17 @@ async def user_register(request):
 
     if 'user_name' not in jsonData:
         return json({'error': 'Please enter your name'}, 400)
-    else:
-        user_name = jsonData['user_name']
-
     if 'user_email' not in jsonData:
         return json({'error': 'Please specify your email to login to Scrypt'}, 400)
-    else:
-        user_email = jsonData['user_email']
-
     if 'password' not in jsonData:
         return json({'error': 'Please enter a safe password to login to your Scrypt account'}, 400)
-    else:
-        password = jsonData['password']
-
     if 'picture' not in jsonData:
         return json({'error': 'Please choose an avatar for your Scrypt profile'}, 400)
-    else:
-        picture = jsonData['picture']
+
+    user_name = jsonData['user_name']
+    user_email = jsonData['user_email']
+    password = jsonData['password']
+    picture = jsonData['picture']
 
     return scrypt_user_client.user_register(user_name, user_email, password, picture)
 
@@ -38,13 +32,11 @@ async def user_login(request):
 
     if 'user_email' not in jsonData:
         return json({'error': 'Please enter the email associated with your Scrypt account'}, 400)
-    else:
-        user_email = jsonData['user_email']
-    
     if 'password' not in jsonData:
         return json({'error': 'Please enter your password to login to Scrypt'}, 400)
-    else:
-        password = jsonData['password']
+
+    user_email = jsonData['user_email']
+    password = jsonData['password']
 
     return scrypt_user_client.user_login(user_email, password)
 
@@ -53,7 +45,7 @@ async def user_logout(request):
 
     if 'api_token' not in jsonData:
         return json({'error': 'Could not authenticate'}, 400)
-    else:
-        api_token = jsonData['api_token']
+
+    api_token = jsonData['api_token']
 
     return scrypt_user_client.user_logout(api_token)
